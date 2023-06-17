4bec3ffbddad912617fd22f8107ab5a515dcb4b3
vkomlev <victor.v.komlev@gmail.com>
2023-06-17 12:23:29 +0500

    commit2

--- app/src/main/java/com/example/mapkit20/MainActivity.java
+++ app/src/main/java/com/example/mapkit20/MainActivity.java
@@ -6,19 +6,25 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Circle;
 import com.yandex.mapkit.geometry.LinearRing;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.geometry.Polygon;
 import com.yandex.mapkit.geometry.Polyline;
 import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CircleMapObject;
 import com.yandex.mapkit.map.CompositeIcon;
 import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.MapObject;
 import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.MapObjectTapListener;
 import com.yandex.mapkit.map.PlacemarkMapObject;
+import com.yandex.mapkit.map.PolygonMapObject;
 import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
 import com.yandex.mapkit.user_location.UserLocationLayer;
@@ -28,10 +34,12 @@ import com.yandex.runtime.image.ImageProvider;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
     private final String MAPKIT_API_KEY = "6953bbfa-0280-46af-a53b-56ece8baa13a";
     private final Point TARGET_LOCATION = new Point(51.2049, 58.5668);
+    private final Point CIRCLE_CENTER = new Point(51.2049, 59.5668);
     private MapView mapView;
     private PlacemarkMapObject Mark;
     private UserLocationLayer userLocationLayer;
@@ -55,19 +63,67 @@ public class MainActivity extends AppCompatActivity implements UserLocationObjec
         userLocationLayer.setObjectListener((UserLocationObjectListener) this);
         mapView.getMap().getMapObjects().addPlacemark(new Point(51.207970, 58.563277)).setIcon(ImageProvider.fromResource(this, R.drawable.icon));
         List<Point> points = new ArrayList<>();
-        points.add(new Point(51.207970, 58.563277));
-        points.add(new Point(51.307970, 58.663277));
-        points.add(new Point(51.407970, 58.763277));
-        points.add(new Point(51.507970, 58.863277));
+        points.add(new Point(55.207970, 56.563277));
+        points.add(new Point(53.307970, 59.663277));
+        points.add(new Point(52.407970, 60.763277));
+        points.add(new Point(51.507970, 62.863277));
         mapObjectCollection = mapView.getMap().getMapObjects().addCollection();
         Polyline poly = new Polyline(points);
-        Polygon polygon = new Polygon(new LinearRing(points));
-        mapObjectCollection.addPolyline(poly);
-        mapObjectCollection.addPolygon(polygon);
+        //mapObjectCollection.addPolyline(poly);
+        PolygonMapObject rect = mapObjectCollection.addPolygon(new Polygon(new LinearRing(points), new ArrayList<LinearRing>()));
 
 
         //Polygon polygon = Polygon()
     }
+    private MapObjectTapListener circleMapObjectTapListener = new MapObjectTapListener() {
+        @Override
+        public boolean onMapObjectTap(MapObject mapObject, Point point) {
+            if (mapObject instanceof CircleMapObject) {
+                CircleMapObject circle = (CircleMapObject)mapObject;
+
+                float randomRadius = 100.0f + 50.0f * new Random().nextFloat();
+
+                Circle curGeometry = circle.getGeometry();
+                Circle newGeometry = new Circle(curGeometry.getCenter(), randomRadius);
+                circle.setGeometry(newGeometry);
+
+                Object userData = circle.getUserData();
+                if (userData instanceof CircleMapObjectUserData) {
+                    CircleMapObjectUserData circleUserData = (CircleMapObjectUserData)userData;
+
+                    Toast toast = Toast.makeText(
+                            getApplicationContext(),
+                            "Circle with id " + circleUserData.id + " and description '"
+                                    + circleUserData.description + "' tapped",
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+            return true;
+        }
+    };
+
+    private class CircleMapObjectUserData {
+        final int id;
+        final String description;
+
+        CircleMapObjectUserData(int id, String description) {
+            this.id = id;
+            this.description = description;
+        }
+    }
+
+    private void createTappableCircle() {
+        CircleMapObject circle = mapObjectCollection.addCircle(
+                new Circle(CIRCLE_CENTER, 100), Color.GREEN, 2, Color.RED);
+        circle.setZIndex(100.0f);
+        circle.setUserData(new CircleMapObjectUserData(42, "Tappable circle"));
+
+        // Client code must retain strong reference to the listener.
+        circle.addTapListener(circleMapObjectTapListener);
+    }
+
+
     @Override
     protected void onStop() {
         mapView.onStop();
