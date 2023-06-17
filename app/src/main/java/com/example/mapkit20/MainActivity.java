63ea30868953cd46f26c0a653bc056b1c1ad5936
vkomlev <victor.v.komlev@gmail.com>
2023-06-17 11:48:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mapkit20/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.mapkit20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.os.Bundle;
+
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.LinearRing;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.geometry.Polygon;
+import com.yandex.mapkit.geometry.Polyline;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.PlacemarkMapObject;
+import com.yandex.mapkit.map.RotationType;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.image.ImageProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
+    private final String MAPKIT_API_KEY = "6953bbfa-0280-46af-a53b-56ece8baa13a";
+    private final Point TARGET_LOCATION = new Point(51.2049, 58.5668);
+    private MapView mapView;
+    private PlacemarkMapObject Mark;
+    private UserLocationLayer userLocationLayer;
+    private Context context;
+    private ImageProvider iProvider;
+    private MapObjectCollection mapObjectCollection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().setRotateGesturesEnabled(true);
+        mapView.getMap().move(new CameraPosition(new Point(0, 0), 14, 0, 0));
+        MapKit mapKit = MapKitFactory.getInstance();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener((UserLocationObjectListener) this);
+        mapView.getMap().getMapObjects().addPlacemark(new Point(51.207970, 58.563277)).setIcon(ImageProvider.fromResource(this, R.drawable.icon));
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(51.207970, 58.563277));
+        points.add(new Point(51.307970, 58.663277));
+        points.add(new Point(51.407970, 58.763277));
+        points.add(new Point(51.507970, 58.863277));
+        mapObjectCollection = mapView.getMap().getMapObjects().addCollection();
+        Polyline poly = new Polyline(points);
+        mapObjectCollection.addPolyline(poly);
+
+
+        //Polygon polygon = Polygon()
+    }
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+    public void onObjectAdded(UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_arrow));
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+        pinIcon.setIcon(
+                "icon",
+                ImageProvider.fromResource(this, R.drawable.icon),
+                new IconStyle().setAnchor(new PointF(0f, 0f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(0f)
+                        .setScale(1f)
+        );
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.search_result),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+    }
+    public void onObjectRemoved(UserLocationView view) {
+    }
+    public void onObjectUpdated(UserLocationView view, ObjectEvent event) {
+    }
+
+}
